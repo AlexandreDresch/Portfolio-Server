d20801073f469ff8643e70a8069fbb873ccbe546
AlexandreDresch <connectWithAlexandre@proton.me>
2024-03-27 23:59:41 -0300

    feat: add CreateProjectDto

--- src/project/dto/create-project.dto.ts
+++ src/project/dto/create-project.dto.ts
@@ -1,1 +1,38 @@
-export class CreateProjectDto {}
+import { ProjectType } from '@prisma/client';
+import {
+  IsBoolean,
+  IsNotEmpty,
+  IsString,
+  IsArray,
+  ArrayMinSize,
+} from 'class-validator';
+
+export class CreateProjectDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  description: string;
+
+  @IsBoolean()
+  done: boolean;
+
+  @IsArray()
+  @IsString({ each: true })
+  @ArrayMinSize(1)
+  images: string[];
+
+  deployment_url?: string;
+
+  @IsString()
+  @IsNotEmpty()
+  github_url: string;
+
+  @IsString()
+  @IsNotEmpty()
+  date: string;
+
+  type: ProjectType;
+}
