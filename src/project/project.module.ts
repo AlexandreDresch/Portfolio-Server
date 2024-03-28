e37e3921eaf42af8b3bbbac46aea9ba3b0576714
AlexandreDresch <connectWithAlexandre@proton.me>
2024-03-28 00:08:38 -0300

    feat: update ProjectModule providers

--- src/project/project.module.ts
+++ src/project/project.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ProjectService } from './project.service';
 import { ProjectController } from './project.controller';
+import { ProjectRepository } from './repository/project.repository';
+import { PrismaProjectRepository } from './repository/implementations/prismaProject.repository';
 
 @Module({
   controllers: [ProjectController],
-  providers: [ProjectService],
+  providers: [
+    ProjectService,
+    { provide: ProjectRepository, useClass: PrismaProjectRepository },
+  ],
 })
 export class ProjectModule {}
