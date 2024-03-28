d658a8f93e41c7eb442440c7993e094c2ddf00eb
AlexandreDresch <connectWithAlexandre@proton.me>
2024-03-28 00:04:31 -0300

    feat: add PrismaModule to AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ProjectModule } from './project/project.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [ProjectModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [PrismaModule, ProjectModule],
 })
 export class AppModule {}
