61bebf8e76a25766e6c21d0c037637824d2ad687
AlexandreDresch <connectWithAlexandre@proton.me>
2024-03-29 21:13:02 -0300

    feat: add update & remove to projects repository

--- src/project/repository/project.repository.ts
+++ src/project/repository/project.repository.ts
@@ -1,8 +1,14 @@
 import { Project } from '@prisma/client';
 import { CreateProjectDto } from 'src/project/dto/create-project.dto';
+import { UpdateProjectDto } from '../dto/update-project.dto';
 
 export abstract class ProjectRepository {
   abstract create(createProjectDto: CreateProjectDto): Promise<Project>;
   abstract findAll(): Promise<Project[]>;
   abstract findByName(name: string): Promise<Project>;
+  abstract update(
+    id: number,
+    updateProjectDto: UpdateProjectDto,
+  ): Promise<Project>;
+  abstract remove(id: number): Promise<void>;
 }
