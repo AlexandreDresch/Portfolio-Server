89b5d8af7bff494ac2f13f0bc8848f8a8cbd4c53
AlexandreDresch <connectWithAlexandre@proton.me>
2024-03-29 21:14:19 -0300

    feat: add update & remove, and Injectable decorator

--- src/project/repository/implementations/prismaProject.repository.ts
+++ src/project/repository/implementations/prismaProject.repository.ts
@@ -2,7 +2,10 @@ import { PrismaService } from 'src/prisma/prisma.service';
 import { ProjectRepository } from '../project.repository';
 import { CreateProjectDto } from 'src/project/dto/create-project.dto';
 import { Project } from '@prisma/client';
+import { UpdateProjectDto } from 'src/project/dto/update-project.dto';
+import { Injectable } from '@nestjs/common';
 
+@Injectable()
 export class PrismaProjectRepository implements ProjectRepository {
   constructor(private readonly prisma: PrismaService) {}
 
@@ -12,10 +15,24 @@ export class PrismaProjectRepository implements ProjectRepository {
   }
 
   async findAll(): Promise<Project[]> {
-    return this.prisma.project.findMany();
+    return await this.prisma.project.findMany();
   }
 
   async findByName(name: string): Promise<Project> {
     return await this.prisma.project.findFirst({ where: { name: name } });
+  }
+
+  async update(
+    id: number,
+    updateProjectDto: UpdateProjectDto,
+  ): Promise<Project> {
+    return await this.prisma.project.update({
+      where: { id: id },
+      data: updateProjectDto,
+    });
+  }
+
+  async remove(id: number): Promise<void> {
+    await this.prisma.project.delete({ where: { id: id } });
   }
 }
